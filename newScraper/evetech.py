9998ca46c1829c5b26838cbc2abbd596156be03b
2302891 <gavinchen2001@gmail.com>
2023-12-10 23:55:21 +0200

    changes on the structure of files

---
+++ newScraper/evetech.py
@@ -0,0 +1,142 @@
+from selenium import webdriver as wd
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+import time 
+from selenium.webdriver.support.ui import Select
+from datetime import datetime
+from selenium.webdriver.common.action_chains import ActionChains
+import requests
+import csv
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from datetime import datetime
+
+# datetime object containing current date and time
+now = datetime.now()
+
+# dd/mm/YY H:M:S
+dt_string = now.strftime("%d-%m-%Y_%H%M")
+
+# Set up Chrome options for headless mode and different user agent
+chrome_options = Options()
+chrome_options.add_argument('--headless')  # Enable headless mode
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36')
+
+# Create a webdriver instance with the specified options
+driver = webdriver.Chrome(options=chrome_options)
+
+# Navigate to the desired website
+url = 'https://www.evetech.co.za/components.aspx'
+driver.get(url)
+
+all_data = []
+
+def scrape_site(driver,categories):
+    time.sleep(5)
+    # Get the page source (HTML content)
+    page_source = driver.page_source
+
+    # Use BeautifulSoup to parse the HTML
+    soup = BeautifulSoup(page_source, 'html.parser')
+    
+    #Product category
+    product_category = soup.find('div',class_='d-block cols-12 gap-2 gap-sm-3 comp-top-section')
+    product_category = product_category.findAll('div', class_='detail')
+    get_len_of_product_category = len(product_category)
+    if len(product_category) < 1:
+        product_category = categories
+    else:
+        product_category = product_category[get_len_of_product_category-1].find('h1').text
+
+    # Find the number of product
+    products = soup.findAll('div', class_='ComponentCard_Products__Card__SJT5q ComponentCard_HoverGrow__Q2lEZ shadow overflow-hidden h-100 gap-2 position-relative card')
+    print("Number of products:", len(products))
+
+    for i in products:
+        # Get the product name and price and availability 
+        product_names = i.find('h3', class_="fs-6 fw-2 lh-1 m-0 overflow-hidden h-100").text.strip()
+        product_price = int(i.find('div', class_="ComponentCard_Products__Price__SG2mn fw-3 fs-3 flex-shrink-0").text.replace("R ", "").strip())
+
+        product_availiable_text = str(i.find('span', class_="fw-1 fs-6 text-wrap").text.strip())
+        # print(product_availiable_text)
+        product_availiablity = True if product_availiable_text.__contains__("In Stock") else False
+
+        all_data.append([product_names, product_price, product_availiablity, product_category])
+        # print(product_names, ": ", product_price, ": ", product_availiablity,": ", product_category)
+
+# Get the page source (HTML content)
+page_source = driver.page_source
+
+# Use BeautifulSoup to parse the HTML
+soup = BeautifulSoup(page_source, 'html.parser')
+
+# Find all categories button 
+categories = soup.findAll('div', class_='Components_Child__mYntX Components_HoverGrow__br6Zs position-relative')
+page_positioin = 250
+
+for i, category in enumerate(categories):
+    # Find all buttons within the category div
+    buttons = category.find_all('button', class_='rounded-pill bg-gradient lh-1 border border-primary btn btn-light btn-sm')
+
+    # Check if there is a button with view all
+    button_view_all = category.find('button', class_='rounded-pill bg-gradient lh-1 btn btn-primary btn-sm')
+
+    # Check if the button is not None
+    if button_view_all is not None:
+        get_number_of_buttons = len(buttons)
+        # You can construct XPath using the button class if needed
+        button_xpath = f'//*[@id="root"]/div/div[2]/div/section[2]/div/div/div[{i+1}]/div[1]/div[1]/button[{get_number_of_buttons+1}]'
+
+        # Scroll to the button
+        driver.execute_script(f"window.scrollTo(0, {str(page_positioin)});")
+        time.sleep(5)
+
+        # Click the button
+        driver.find_element(By.XPATH, button_xpath).click()
+        print("Scraping:", driver.current_url)
+        # Scrape the site
+        scrape_site(driver, button_view_all.text)
+        time.sleep(3)
+
+        # Go back to the previous page and scroll to the button
+        driver.execute_script("window.history.go(-1)")
+        time.sleep(5)
+        driver.execute_script(f"window.scrollTo(0, {str(page_positioin)});")
+        time.sleep(5)
+    else:
+        for k, button in enumerate(buttons):
+            # You can construct XPath using the button class if needed
+            button_xpath = f'//*[@id="root"]/div/div[2]/div/section[2]/div/div/div[{i+1}]/div[1]/div[1]/button[{k+1}]'
+
+            # Scroll to the button
+            driver.execute_script(f"window.scrollTo(0, {str(page_positioin)});")
+            time.sleep(7)
+
+            # Click the button
+            driver.find_element(By.XPATH, button_xpath).click()
+            print("Scraping:", driver.current_url)
+            # Scrape the site
+            scrape_site(driver, button.text)
+            time.sleep(3)
+
+            # Go back to the previous page and scroll to the button
+            driver.execute_script("window.history.go(-1)")
+            time.sleep(5)
+            driver.execute_script(f"window.scrollTo(0, {str(page_positioin)});")
+            time.sleep(5)
+    
+    # Get the page position
+    if (i+1) % 3 == 0:
+        page_positioin += 250
+        
+    i += 1
+
+# Close the browser
+driver.quit()
+
+# Write the data to csv file
+with open(f'../Products/{dt_string}_evetech.csv', 'w', newline='', encoding='utf-8') as f:
+    writer = csv.writer(f)
+    writer.writerow(['Title','Price','In Stock','Category'])
+    writer.writerows(all_data)
---
+++ newScraper/takealot.py
@@ -0,0 +1,103 @@
+from selenium import webdriver as wd
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+import time 
+from selenium.webdriver.support.ui import Select
+from datetime import datetime
+from selenium.webdriver.common.action_chains import ActionChains
+import requests
+import csv
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from datetime import datetime
+
+# datetime object containing current date and time
+now = datetime.now()
+
+# dd/mm/YY H:M:S
+dt_string = now.strftime("%d-%m-%Y_%H%M")
+
+# All the links to the categories
+All_links = [
+    ['https://www.takealot.com/computers/cpu-26419','CPU'],
+    ['https://www.takealot.com/computers/motherboards-26423','Motherboard'],
+    ['https://www.takealot.com/computers/power-supplies-26425','Power Supplies'],
+    ['https://www.takealot.com/computers/computer-memory-ram-26418','RAM'],
+    ['https://www.takealot.com/computers/graphics-cards-26421','Graphics Card'],
+    ['https://www.takealot.com/all?_sb=1&_r=1&qsearch=HDD&via=suggestions&_si=bc15f929dc10b77b309874df29ef4558','HDD'],
+    ['https://www.takealot.com/all?_sb=1&_r=1&qsearch=SSD&via=suggestions&_si=bc15f929dc10b77b309874df29ef4558','SSD'],
+    ['https://www.takealot.com/all?_sb=1&_r=1&qsearch=NVME&via=suggestions&_si=bc15f929dc10b77b309874df29ef4558','NVME'],
+    ['https://www.takealot.com/all?_sb=1&_r=1&qsearch=SATA&via=suggestions&_si=bc15f929dc10b77b309874df29ef4558','SATA'],
+    ['https://www.takealot.com/computers/cases-and-chassis-26416','Chasis'],
+    ['https://www.takealot.com/computers/interface-cards-26422','Interface Cards'],
+    ['https://www.takealot.com/computers/sound-cards-26426','Sound Cards'],
+    ['https://www.takealot.com/computers/optical-drives-26424','Optical Drives'],
+    ['https://www.takealot.com/computers/fans-and-cooling-26420','Fans and Cooling'],
+    ['https://www.takealot.com/computers/computer-monitors-27167','Monitor'],
+    ['https://www.takealot.com/computers/mouse-and-presentation-26412','Mouse'],
+    ['https://www.takealot.com/computers/keyboards-26410','Keyboard'],
+    ['https://www.takealot.com/computers/headsets-and-microphones-26409','Headset'],
+    ['https://www.takealot.com/computers/computer-speakers-26407','Speakers'],
+    ['https://www.takealot.com/computers/webcams-26413','Webcam'],
+    ['https://www.takealot.com/computers/mouse-pads-26404','Mouse Pad']
+]
+
+# Create csv file
+all_data = []
+
+# Set up Chrome options for headless mode and different user agent
+chrome_options = Options()
+chrome_options.add_argument('--headless')  # Enable headless mode
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument('user-agent=AdsBot-Google')
+
+# Create a webdriver instance with the specified options
+driver = webdriver.Chrome(options=chrome_options)
+
+# Iterate through the categories
+for i in All_links:
+    URL = i[0]
+    Category = i[1]
+
+    # Fetch the URL
+    driver.get(URL)
+
+    # Wait for the page to fully load
+    time.sleep(5)   
+    while True:
+        # Scroll down to bottom
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(3)
+        # Wait to load page
+        if driver.find_elements(By.XPATH, '//button[@class="button ghost search-listings-module_load-more_OwyvW"]')==[]:
+            break
+        # find the load more button
+        load_more_button = driver.find_element(By.XPATH, '//button[@class="button ghost search-listings-module_load-more_OwyvW"]')
+        load_more_button.click()
+        time.sleep(3)
+    
+    # Get the page source (HTML content)
+    page_source = driver.page_source
+
+    # Use BeautifulSoup to parse the HTML
+    soup = BeautifulSoup(page_source, 'html.parser')
+
+    # Find all product 
+    all_product = soup.findAll('div', class_="product-card product-card-module_product-card_fdqa8")
+    time.sleep(5)
+    for j in all_product:
+        price = j.find('li', class_="price product-card-module_price_zVU6d").text.strip().replace('R ','').replace(',','')
+        name = j.find('h4', class_="product-title").text.strip()
+        if j.find('div', class_="cell shrink stock-availability-status") != None:
+            availability = j.find('div', class_="cell shrink stock-availability-status").text.strip()
+        else:
+            availability = 'In Stock'
+        all_data.append([name,price,availability,Category])
+    print('Scraping '+Category+' with '+str(len(all_product))+' products :'+URL)
+driver.quit()
+
+# Write the data to csv file
+with open(f'../Products/{dt_string}_takealot.csv', 'w', newline='', encoding='utf-8') as f:
+    writer = csv.writer(f)
+    writer.writerow(['Title','Price','In Stock','Category'])
+    writer.writerows(all_data)
